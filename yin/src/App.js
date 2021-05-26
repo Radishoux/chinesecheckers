9eb8f790fbeea7b92a9aab8b628ddc93251ec737
Rudy <rudy.quinternet@gmail.com>
2021-05-27 01:36:13 +0200

    minipush

--- yin/src/App.js
+++ yin/src/App.js
@@ -64,7 +64,7 @@ export default function App() {
 
     function Extra() {
         return (
-            <div style={{"background":"rgba(255,255,255,0.7)", "height":"20vh", "width":"50vw", "margin":"50px", "borderRadius":"10px"}}>
+            <div style={{"background":"rgba(255,255,255,0.7)", "height":"150px", "width":"50vw", "margin":"10px", "borderRadius":"10px"}}>
             {room === null || room === undefined || room === "" ?
             <h2 className="text-4xl font-bold mt-20">Regles</h2>
             :
